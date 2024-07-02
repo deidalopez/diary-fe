b419af3e30274699f982a0d49348a1aa044f63fa
Deida Lopez <deidalopez1@gmail.com>
2024-07-01 22:10:09 -0700

    Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import "./App.css";
 import { useRoutes } from "react-router-dom";
 import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import PostPage from "./pages/PostPage";
 
 function App() {
-  const elements = useRoutes([{ path: "/", element: <Layout /> }]);
+  const elements = useRoutes([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/api/posts/:id", element: <PostPage /> },
+      ],
+    },
+  ]);
   return elements;
   // <div>
   //   <header>
