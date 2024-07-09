ce6caa8d1786f542580d24183490356ca5b7d4de
Deida Lopez <deidalopez1@gmail.com>
2024-07-08 19:44:27 -0700

    Forgot to include dependency array in AuthContext useEffect

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -28,7 +28,7 @@ export const AuthContextProvider = ({ children }) => {
     if (user) {
       dispatch({ type: "LOGIN", payload: user });
     }
-  });
+  }, []);
 
   console.log("AuthContext", state);
   return (
