919efde1a076dec58538dc618d5c2f10b323af6b
Deida Lopez <deidalopez1@gmail.com>
2024-07-08 21:23:39 -0700

    Cleanup SignUp page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,45 +1,43 @@
 import { useForm } from "react-hook-form";
-import useSignUp from "../hooks/useSignUp";
-
+import useSignup from "../hooks/useSignUp";
 import styles from "../styles/styles.module.scss";
 
-const Login = () => {
+const Signup = () => {
   const {
     register,
     handleSubmit,
     reset,
     formState: { errors },
   } = useForm();
-
-  const { signup, loading, error } = useSignUp();
+  const { signup, loading, error } = useSignup();
 
   const onSubmit = async (data) => {
     await signup(data.email, data.password);
     reset({ email: "", password: "" });
   };
+
   return (
-    <div>
-      <form className={styles.loginForm} onSubmit={handleSubmit(onSubmit)}>
-        <h2>SIGN UP</h2>
-        <input
-          type="email"
-          placeholder="email"
-          {...register("email", { required: "Required field" })}
-        />
-        <p>{errors.email?.message}</p>
-        <input
-          type="password"
-          placeholder="password"
-          {...register("password", { required: "Required field" })}
-        />
-        <p>{errors.password?.message}</p>
-        <button className={styles.submit} type="submit" disabled={loading}>
-          Log In
-        </button>
-        {error && <div>{error}</div>}
-      </form>
-    </div>
+    <form className={styles.loginForm} onSubmit={handleSubmit(onSubmit)}>
+      <h3> Sign Up </h3>
+      <input
+        type="email"
+        {...register("email", { required: "required field" })}
+        placeholder="email"
+        autoComplete="off"
+      />
+      <p>{errors.email?.message}</p>
+      <input
+        type="password"
+        {...register("password", { required: "required field" })}
+        placeholder="password"
+      />
+      <p>{errors.password?.message}</p>
+      <button className={styles.submit} type="submit" disabled={loading}>
+        Sign Up
+      </button>
+      {error && <p>{error}</p>}
+    </form>
   );
 };
 
-export default Login;
+export default Signup;
